8eec4c7f721bcedc81a9164701ed0f1ceefb2c0e
Jay Ghosh <jayghosh730@gmail.com>
2023-03-29 17:28:29 -0400

    api routes started

--- backend/core_api/main.py
+++ backend/core_api/main.py
@@ -5,16 +5,42 @@ import os
 
 load_dotenv()
 app = Flask(__name__)
-api = Api(app, doc="/docs")
+api = Api(app, doc="/")
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-@api.route('/hello')
-class HelloWorld(Resource): 
+@api.route('/all_hotels')
+class Hotels(Resource):
     def get(self): 
-        return {"message": "Hello World"}
+        return {"message": 'Hotels'}
+
+@api.route('/hotel_chain/<int:id>')
+class Hotel_Chain(Resource): 
+    def get(self, id): 
+        return {"message": f"Getting hotel chain with id {id}"}
+
+    def post(self, id): 
+        return {"message":  f"Adding hotel chain with id {id}"}
+
+    def delete(self, id): 
+        return {"message":  f"Deleting hotel chain with id {id}"}
+
+@api.route('/hotel_chain/all_hotels/<int:id>')
+class Hotel_Chain(Resource): 
+    def get(self, id): 
+        return {"message": f"Getting all hotels associated with hotel chain {id}"}
 
 
 
+@api.route('/hotel/<int:id>')
+class Hotels(Resource): 
+    def get(self, id): 
+        return {"message": f"Getting hotel with id {id}"}
+
+    def post(self, id): 
+        return {"message":  f"Adding hotel with id {id}"}
+
+    def delete(self, id): 
+        return {"message":  f"Deleting hotel with id {id}"}
 
 if __name__ == '__main__': 
-    app.run()
+    app.run(debug=True)
