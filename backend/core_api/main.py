2675f1ceb3bcb972df9cbd168d6d997b5c8037f7
Jay Ghosh <jayghosh730@gmail.com>
2023-03-29 23:59:12 -0400

    api edit

--- backend/core_api/main.py
+++ backend/core_api/main.py
@@ -1,17 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 app = Flask(__name__)
-api = Api(app, doc="/")
+api = Api(app, doc="/docs")
 SECRET_KEY = os.getenv('SECRET_KEY')
+app.config['SWAGGER_UI_REQUEST_HEADERS'] = {'Content-Type': 'application/json'}
+
+# To specify schema of api payload.
+input_model = api.model('InputModel', {
+    'key': fields.String(required=True, description='Example key'),
+})
 
 @api.route('/all_hotels')
 class Hotels(Resource):
     def get(self): 
-        return {"message": 'Hotels'}
+        data = request.get_json()
+        print(data)
+        to_return = jsonify({"message": 'Hotels', "request": data})
+        return to_return
 
 @api.route('/hotel_chain/<int:id>')
 class Hotel_Chain(Resource): 
@@ -42,13 +52,9 @@ class Hotels(Resource):
 
 @api.route('/hotel/search')
 class HotelSearch(Resource): 
-    def get(self, id): 
+    def get(self): 
+        data = request.get_json()
         return {"message": f"Searching for hotels with provided parameters"}
-
-
-
-
-
 
 if __name__ == '__main__': 
     app.run(debug=True)
